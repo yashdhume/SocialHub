8ca190234e260610ec8906bc917e0ecb041f68f3
aabidmitha10 <46793365+aabidmitha10@users.noreply.github.com>
2020-04-14 18:48:17 -0400

    Integrated SVG logo into splashpage

--- src/components/SplashPageComponents.vue
+++ src/components/SplashPageComponents.vue
@@ -6,8 +6,26 @@
                     <v-col align="center"
                            justify="center"
                            style="height: 1000px">
-
+                        <svg id="mySvg" width="80" height="80">
+                            <defs id="mdef">
+                                <pattern id="image0" x="0" y="0" height="40" width="40">
+                                    <image x="0" y="0" width="20" height="20" xlink:href="http://icons.iconarchive.com/icons/dtafalonso/android-l/256/Youtube-icon.png"></image>
+                                </pattern>
+                                <pattern id="image1" x="0" y="0" height="40" width="40">
+                                    <image x="0" y="0" width="20" height="20" xlink:href="https://s18955.pcdn.co/wp-content/uploads/2018/02/instagramimgnew.png"></image>
+                                </pattern>
+                                <pattern id="image2" x="0" y="0" height="40" width="40">
+                                    <image x="0" y="0" width="20" height="20" xlink:href="https://www.shareicon.net/data/256x256/2017/06/22/887584_logo_512x512.png"></image>
+                                </pattern>
+                                <pattern id="image3" x="0" y="0" height="40" width="40">
+                                    <image x="0" y="0" width="20" height="20" xlink:href="https://img.favpng.com/1/2/24/reddit-logo-youtube-png-favpng-Rukc5hsDFfci1sNk1LkFBccvW_t.jpg"></image>
+                                </pattern>
+                            </defs>
+                        </svg>
+                        <svg id="logoFinal" width="2350" height="300" viewBox="0 0 1000 80"></svg>
+                        <!--
                         <v-img src="@/assets/SocialHubLogoWhite.png" max-width="500" max-height="500"></v-img>
+                        -->
                         <v-container></v-container>
                         <p class="text-center ma-5">All your favorite social media in one place.</p>
 
@@ -22,10 +40,68 @@
 
     </div>
 </template>
+    
+<script src="https://d3js.org/d3.v5.min.js"></script>
 
 <script>
     export default {
-        name: "SplashPageComponents"
+        name: "SplashPageComponents",
+
+        mounted(){
+            let data = [[400, 50, -30, -25, 10], [400, 50, 30, -25, 10], [400, 50, -30, 25, 10], [400, 50, 30, 25, 10], [400, 50, 0, 0, 15]];
+            let data2 = [[400, 50, -30, -25, 6, 6, -10, -10], [400, 50, 30, -25, -6, 6, 10, -10], [400, 50, -30, 25, 6, -6, -10, 10], [400, 50, 30, 25, -6, -6, 10, 10]];
+
+            let height = 500
+            let width = 800
+
+            const svg = d3.select('#logoFinal')
+                          .style("font", "10px sans-serif");
+
+            // Append circles to svg
+            let circles = svg.selectAll("circle")
+                      .data(data)
+                      .enter()
+                      .append("circle");
+
+            // Set circle attributes
+            circles.attr("r", (d, i) => d[4])
+               .attr("cx", (d, i) => d[0])
+               .attr("cy", (d, i) => d[1])
+               .attr("fill", "transparent")
+
+            // Append lines to svg
+            let lines = svg.selectAll("line")
+                      .data(data2)
+                      .enter()
+                      .append("line");
+
+            // Set line attributes
+            lines.attr("x1", (d, i) => d[0]  + d[6])
+               .attr("y1", (d, i) => d[1]  + d[7])
+               .attr("x2", (d, i) => d[0])
+               .attr("y2", (d, i) => d[1])
+
+
+            // Animate the circles on button click
+            // Define animation here
+            d3.selectAll('circle')
+            .transition()
+            .attr('cx', (d, i) => 400 + d[2] )       
+            .attr('cy', (d, i) => 50 + d[3])
+            //.attr('fill', state ? '#1F4357' : '#CDB73A');
+            .attr("stroke", "white")
+            .attr("stroke-width", 3)
+            .style("fill", (d, i) => `url(#image${i})`);
+
+
+            d3.selectAll('line')
+            .transition()
+            .attr('x2', (d, i) => 400 + d[2] + d[4])       
+            .attr('y2', (d, i) => 50 + d[3] + d[5])
+                   .attr("fill", "#e75480")
+            .attr("stroke", "white")
+            .attr("stroke-width", 3);
+        }
     }
 </script>
 
